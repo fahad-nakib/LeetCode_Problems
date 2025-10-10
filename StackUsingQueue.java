c6f33accaa7a8ef12919e877c0f0ea24068145a3
fahad-nakib <fahadnakib27@gmail.com>
2025-10-10 18:39:49 +0600

    Stack Using queue modified

--- StackUsingQueue.java
+++ StackUsingQueue.java
@@ -1,15 +1,16 @@
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Scanner;
 
 public class StackUsingQueue {
     private Queue<Integer> queue;
-    public void MyStack() {
+    public  StackUsingQueue() {
         queue = new LinkedList<>();
     }
 
     public void push(int x) {
         queue.add(x);
-        for (int i=0; i<queue.size(); i++){
+        for (int i=1; i<queue.size(); i++){
             queue.add(queue.remove());
         }
     }
@@ -19,6 +20,9 @@ public class StackUsingQueue {
     }
 
     public int top() {
+        if (queue.isEmpty()){
+            return -1;
+        }
         return queue.peek();
     }
 
@@ -26,12 +30,28 @@ public class StackUsingQueue {
         return queue.isEmpty();
     }
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StackUsingQueue sq = new StackUsingQueue();
         while (true){
+            System.out.println("Current stack : ");
+            System.out.println(sq.queue);
             System.out.println("Enter the choice : ");
             System.out.println("1. Push Element to queue.");
             System.out.println("2. Remove Element to queue.");
             System.out.println("3. Peek top Element to queue.");
             System.out.println("4. Check queue size.");
+            int choice = sc.nextInt();
+            switch (choice){
+                case 1 :
+                    System.out.println("Enter value : ");
+                    sq.push(sc.nextInt());
+                    break;
+                case 2 : sq.pop(); break;
+                case 3 :
+                    System.out.println(sq.top()); break;
+                case 4 :
+                    System.out.println(sq.empty()); break;
+            }
         }
     }
 }
